d638a04db7adb5a2f3ed692cb046f99f9ca57b4c
wangxue <3208230974@qq.com>
2022-08-20 15:01:40 +0800

    框架修改

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,13 @@
+/*
+ * @Author: wangxue 3208230974@qq.com
+ * @Date: 2022-08-20 10:27:12
+ * @LastEditors: wangxue 3208230974@qq.com
+ * @LastEditTime: 2022-08-20 14:57:06
+ * @FilePath: \my-vite-app\src\router\index.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
-import Layout from "../Layout/layout.vue";
+import Layout from "@/Layout/layout.vue";
 const routes: Array<RouteRecordRaw> = [
     {
         path: "/",
--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@
  * @Author: wangxue 3208230974@qq.com
  * @Date: 2022-08-20 09:26:26
  * @LastEditors: wangxue 3208230974@qq.com
- * @LastEditTime: 2022-08-20 09:52:51
+ * @LastEditTime: 2022-08-20 14:25:25
  * @FilePath: \my-vite-app\vite.config.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -14,16 +14,21 @@ export default (config: UserConfig): UserConfigExport => {
   const mode = config.mode as string;
   const isEnvProd = ['production', 'test'].includes(mode)
   return defineConfig({
+    base: "./",
     plugins: [
       vue()
   ],
   resolve: {
-    alias: [
-      {
-        find: '@/',
-        replacement: resolve(__dirname, 'src')
-      }
-    ]
+    alias: {
+      // 配置别名
+      "@": resolve(__dirname, "./src"),
+    }
   },
+  server: {
+    open: true, // 自动启动浏览器
+    host: "0.0.0.0", // localhost
+    port: 8001, // 端口号
+    https: false
+  }
   })
 }
